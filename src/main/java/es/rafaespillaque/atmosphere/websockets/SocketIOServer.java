25f0ac51488a8abdffd989a751c82f8d4357c351
Rafaesp <rafaespillaque@gmail.com>
2012-10-16 19:32:40 +0200

    Websockets con Atmosphere y usando Redis para guardar UUIDS

---
+++ src/main/java/es/rafaespillaque/atmosphere/websockets/SocketIOServer.java
@@ -0,0 +1,20 @@
+/*package es.rafaespillaque.atmosphere.websockets;
+
+import java.io.IOException;
+
+import org.atmosphere.nettosphere.Config;
+import org.atmosphere.nettosphere.Nettosphere;
+
+public class SocketIOServer {
+
+    public static void main(String[] args) throws IOException {
+        new Nettosphere.Builder().config(
+                new Config.Builder()
+                        .port(8080)
+                        .host("127.0.0.1")
+                        .resource(WebSocketChat.class)
+                        .build())
+                .build().start();
+    }   
+}
+*/
---
+++ src/main/java/es/rafaespillaque/atmosphere/websockets/WebSocketChat.java
@@ -0,0 +1,59 @@
+package es.rafaespillaque.atmosphere.websockets;
+
+import java.io.IOException;
+
+import org.atmosphere.config.service.WebSocketHandlerService;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.websocket.WebSocket;
+import org.atmosphere.websocket.WebSocketHandler;
+import org.codehaus.jackson.JsonFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@WebSocketHandlerService
+public class WebSocketChat extends WebSocketHandler {
+	
+	private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+
+    public WebSocketChat(){
+    }
+    
+    @Override
+    public void onOpen(WebSocket ws) {
+    	System.out.println("onOpen");
+        ws.resource().setBroadcaster(
+              BroadcasterFactory.getDefault().lookup("broadcaster", true));
+        Jedis jedis = new Jedis("localhost");
+        try {
+        	String uuid = ws.resource().uuid();
+			ws.write("{uuid:"+uuid+"}");
+			jedis.set(uuid, "init");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("IOException en onOpen");
+		} finally{
+			pool.returnResource(jedis);
+		}
+        ws.resource().suspend();
+    }
+
+    public void onTextMessage(WebSocket ws, String message) {
+    	System.out.println("onText: "+message);
+    	
+        AtmosphereResource r = ws.resource();
+        Broadcaster b = r.getBroadcaster();
+        b.broadcast(message);
+    }
+
+	@Override
+	public void onClose(WebSocket ws) {
+		System.out.println("onClose");
+		
+	}
+    
+    
+}
