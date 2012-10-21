c1fdb1fcca30ab2079156eb11e8bdd2f4593aeaf
Rafaesp <rafaespillaque@gmail.com>
2012-10-21 19:16:21 +0200

    Arreglado pom

--- src/main/java/es/rafaespillaque/atmosphere/websockets/WebSocketChat.java
+++ src/main/java/es/rafaespillaque/atmosphere/websockets/WebSocketChat.java
@@ -8,16 +8,11 @@ import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.websocket.WebSocket;
 import org.atmosphere.websocket.WebSocketHandler;
-import org.codehaus.jackson.JsonFactory;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 @WebSocketHandlerService
 public class WebSocketChat extends WebSocketHandler {
 	
-	private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+//	private static final JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
 
     public WebSocketChat(){
     }
@@ -27,16 +22,16 @@ public class WebSocketChat extends WebSocketHandler {
     	System.out.println("onOpen");
         ws.resource().setBroadcaster(
               BroadcasterFactory.getDefault().lookup("broadcaster", true));
-        Jedis jedis = new Jedis("localhost");
+//        Jedis jedis = new Jedis("localhost");
         try {
         	String uuid = ws.resource().uuid();
 			ws.write("{uuid:"+uuid+"}");
-			jedis.set(uuid, "init");
+//			jedis.set(uuid, "init");
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("IOException en onOpen");
 		} finally{
-			pool.returnResource(jedis);
+//			pool.returnResource(jedis);
 		}
         ws.resource().suspend();
     }
